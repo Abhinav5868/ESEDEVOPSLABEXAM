73979cdf745fde19481fa479599d45c61dd0bd54
Abhinav5868 <abhinav2003.d@gmail.com>
2024-11-08 09:34:46 +0530

    Add Java files in application and test directories

---
+++ src/test/java/com/example/demo/ApplicationTest.java
@@ -0,0 +1,16 @@
+// src/test/java/com/example/demo/ApplicationTests.java
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTests {
+    
+    @Test
+    public void testMessage() {
+        Application app = new Application();
+        assertEquals("Hello, CI/CD Pipeline!", app.getMessage());
+    }
+}
