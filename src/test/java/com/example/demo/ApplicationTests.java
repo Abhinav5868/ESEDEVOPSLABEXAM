58829c4f63c1f01ef1326579c7b8d0091ad314ed
Abhinav5868 <abhinav2003.d@gmail.com>
2024-11-08 10:18:01 +0530

    Update ApplicationTests.java

--- src/test/java/com/example/demo/ApplicationTests.java
+++ src/test/java/com/example/demo/ApplicationTests.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class ApplicationTests {
+
     @Test
     public void testMessage() {
         Application app = new Application();
