29e6c02ad563a5c6d04516444d0111472f415a7c
Abhinav5868 <abhinav2003.d@gmail.com>
2024-11-08 10:07:35 +0530

    Update ApplicationTests.java

--- src/test/java/com/example/demo/ApplicationTests.java
+++ src/test/java/com/example/demo/ApplicationTests.java
@@ -1,16 +1,19 @@
-// src/test/java/com/example/demo/ApplicationTests.java
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class ApplicationTests {
-    
+
     @Test
     public void testMessage() {
         Application app = new Application();
         assertEquals("Hello, CI/CD Pipeline!", app.getMessage());
+    }
+
+    @Test
+    void contextLoads() {
     }
 }
